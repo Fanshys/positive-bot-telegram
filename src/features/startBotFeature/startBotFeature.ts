e6a39ac9a0477fb4ccc394149f70eb8a7e9b8e5b
Дмитрий Шабалин <dshabalin@rbc.ru>
2022-09-10 18:34:27 +0500

    handle error

--- src/features/startBotFeature/startBotFeature.ts
+++ src/features/startBotFeature/startBotFeature.ts
@@ -5,13 +5,17 @@ import { registerUser } from './helpers';
 import { StartBotFeatureArgs } from './types';
 
 export const startBotFeature = async ({ bot, chatId, message }: StartBotFeatureArgs): Promise<Message> => {
-  const user = await UsersModel.findOne({ id: message.from.id });
+  try {
+    const user = await UsersModel.findOne({ id: message.from.id });
 
-  if (user) {
-    return bot.sendMessage(chatId, arleadyRegisteredMessage);
+    if (user) {
+      return await bot.sendMessage(chatId, arleadyRegisteredMessage);
+    }
+
+    await registerUser(message);
+
+    return await bot.sendMessage(chatId, successMessage);
+  } catch (error) {
+    return bot.sendMessage(chatId, 'Произошла ошибка');
   }
-
-  await registerUser(message);
-
-  return bot.sendMessage(chatId, successMessage);
 };
